fbf871cd3e821de693c4c55abe29e342f6d617c5
Shoaib Ahmed <153922877+shoaibsmit@users.noreply.github.com>
2024-06-13 21:50:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,107 +1,11 @@
-// const slideImages = [
-//   "https://burgerlab.com.pk/wp-content/uploads/2024/05/app-banner-2.jpg?c062ef&c062ef",
-//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn6q8_0SwnNkM0c0vOT_twL1yzrcvlGuToSw&s",
-//   "https://pixexid.com/api/download/image/a-burger-with-a-sense-of-wanderlust-backpacks-through-a-food-landscape-with-fry-9hbp0rfh.jpeg",
-// ];
-
-// function displaySlider() {
-//   let slidesContainer = document.querySelectorAll(".slides")[0];
-
-//   for (let i = 0; i < slideImages.length; i++) {
-//     let img = `<img src="${slideImages[i]}" alt="Slide ${i + 1}">`;
-//     slidesContainer.innerHTML += img;
-//   }
-// }
-
-// displaySlider();
-
-// const products = [
-//   {
-//     image:
-//       "https://burgerlab.com.pk/wp-content/uploads/2022/01/doppler.png?c062ef&c062ef",
-//     productName: "THE Doppler",
-//     price: "$10.00",
-//   },
-//   {
-//     image:
-//       "https://burgerlab.com.pk/wp-content/uploads/2022/01/quadra.png?c062ef&c062ef",
-//     productName: "Pentra RELOADED",
-//     price: "$20.00",
-//   },
-//   {
-//     image:
-//       "https://burgerlab.com.pk/wp-content/uploads/2022/01/all-american-4.png?c062ef&c062ef",
-//     productName: "ALL PAKISTANI DOUBLE CHEESE",
-//     price: "$50.00",
-//   },
-//   {
-//     image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
-//     productName: "Big Bang",
-//     price: "$40.00",
-//   },
-// ];
-
-// let cart = [
-//   {
-//     image:
-//       "https://burgerlab.com.pk/wp-content/uploads/2022/01/doppler.png?c062ef&c062ef",
-//     productName: "THE Doppler",
-//     price: "$10.00",
-//     qty: 3,
-//     totalPrice: this.price * this.qty,
-//   },
-// ];
-
-// function createProductCard() {
-//   for (let i = 0; i < products.length; i++) {
-//     let productListContainer = document.querySelectorAll(".product-list")[0];
-
-//     let product = products[i];
-//     let productCard = `
-//         <div class="product-item">
-//           <img
-//             src="${product.image}"
-//             alt="${product.productName} image"
-//           />
-//           <h3>${product.productName}</h3>
-//           <p>${product.price}</p>
-//           <p>Add to Cart</p>
-//         </div>
-//     `;
-//     productListContainer.innerHTML += productCard;
-//   }
-// }
-
-// createProductCard();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.js
-
 const slideImages = [
-  "https://burgerlab.com.pk/wp-content/uploads/2024/05/app-banner-2.jpg?c062ef&c062ef",
+  "https://pixexid.com/api/download/image/a-burger-with-a-sense-of-wanderlust-backpacks-through-a-food-landscape-with-fry-9hbp0rfh.jpeg",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn6q8_0SwnNkM0c0vOT_twL1yzrcvlGuToSw&s",
   "https://pixexid.com/api/download/image/a-burger-with-a-sense-of-wanderlust-backpacks-through-a-food-landscape-with-fry-9hbp0rfh.jpeg",
 ];
 
 function displaySlider() {
-  let slidesContainer = document.querySelector(".slides");
+  let slidesContainer = document.querySelectorAll(".slides")[0];
 
   for (let i = 0; i < slideImages.length; i++) {
     let img = `<img src="${slideImages[i]}" alt="Slide ${i + 1}">`;
@@ -113,35 +17,34 @@ displaySlider();
 
 const products = [
   {
-    image:
-      "https://burgerlab.com.pk/wp-content/uploads/2022/01/doppler.png?c062ef&c062ef",
+    id: "502c305a-997e-4534-97b8-f6f7862f3737",
+    image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
     productName: "THE Doppler",
     price: "$10.00",
   },
   {
-    image:
-      "https://burgerlab.com.pk/wp-content/uploads/2022/01/quadra.png?c062ef&c062ef",
+    id: "67aadbfe-a6d9-4d8d-a436-27c369cff5ff",
+    image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
     productName: "Pentra RELOADED",
     price: "$20.00",
   },
   {
-    image:
-      "https://burgerlab.com.pk/wp-content/uploads/2022/01/all-american-4.png?c062ef&c062ef",
+    id: "54c3c17e-b3e9-469b-800a-b065d193a6d9",
+    image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
     productName: "ALL PAKISTANI DOUBLE CHEESE",
     price: "$50.00",
   },
   {
+    id: "e2085e01-f142-4475-8124-e24722cff1d6",
     image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
     productName: "Big Bang",
     price: "$40.00",
   },
 ];
 
-let cart = [];
-
 function createProductCard() {
   for (let i = 0; i < products.length; i++) {
-    let productListContainer = document.querySelector(".product-list");
+    let productListContainer = document.querySelectorAll(".product-list")[0];
 
     let product = products[i];
     let productCard = `
@@ -152,37 +55,77 @@ function createProductCard() {
           />
           <h3>${product.productName}</h3>
           <p>${product.price}</p>
-          <p class="add-to-cart" data-id="${i}">Add to Cart</p>
+          <button onclick="addToCartinObj(${i})">Add to Cart</button>
         </div>
     `;
     productListContainer.innerHTML += productCard;
   }
-
-  const addToCartButtons = document.querySelectorAll('.add-to-cart');
-  addToCartButtons.forEach(button => {
-    button.addEventListener('click', addToCart);
-  });
-}
-
-function addToCart(event) {
-  const productId = event.target.dataset.id;
-  const product = products[productId];
-  
-  const existingProduct = cart.find(item => item.productName === product.productName);
-  
-  if (existingProduct) {
-    existingProduct.qty += 1;
-    existingProduct.totalPrice = `$${parseFloat(existingProduct.totalPrice.slice(1)) + parseFloat(product.price.slice(1))}`;
-  } else {
-    cart.push({
-      ...product,
-      qty: 1,
-      totalPrice: product.price
-    });
-  }
-
-  console.log('Cart:', cart);
 }
 
 createProductCard();
 
+let cart = {};
+
+function addToCartinObj(index) {
+  const { id } = products[index];
+
+  if (id in cart) {
+    cart[id].qty = cart[id].qty + 1;
+    cart[id].totalPrice = cart[id].qty * cart[id].price.slice(1);
+  } else {
+    cart[id] = { ...products[index] };
+    cart[id].qty = 1;
+    cart[id].totalPrice = cart[id].qty * cart[id].price.slice(1);
+  }
+  console.log(cart);
+}
+
+
+function updateTotalPrice() {
+  let totalPrice = 0;
+  for (let item in cart) {
+    totalPrice += cart[item].totalPrice;
+  }
+  document.getElementById('total-price').innerText = `Total Price: $${totalPrice.toFixed(2)}`;
+}
+
+// Add an element in HTML to display the total price
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const totalPriceContainer = document.createElement('div');
+//   totalPriceContainer.id = 'total-price';
+//   totalPriceContainer.innerText = 'Total Price: $0.00';
+//   document.body.appendChild(totalPriceContainer);
+// });
+
+
+
+
+// let cart = [];
+// function addToCartinArr(ind) {
+//   const { id } = products[ind];
+
+//   let matchFound = false;
+//   let targetIndex;
+
+//   cart.forEach((item, index) => {
+//     if (item.id === id) {
+//       matchFound = true;
+//       targetIndex = index;
+//     }
+//   });
+
+//   if (matchFound) {
+//     const product = cart[targetIndex];
+
+//     product.qty = product.qty + 1;
+//     product.totalPrice = product.qty * product.price.slice(1);
+
+//   } else {
+//     let cloneProduct = { ...products[ind] };
+//     cloneProduct.qty = 1;
+//     cloneProduct.totalPrice = cloneProduct.qty * cloneProduct.price.slice(1);
+//     cart.push(cloneProduct);
+//   }
+
+//   console.log(cart);
+// }
